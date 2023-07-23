83c91ffbe8a1bdc22c0c470366dceb10f8c3b6b0
waley-code <oyewaleadeosun@yahoo.com>
2023-07-23 17:30:50 +0100

    complet task 0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""takes two integer arguments page and page_size"""
+
+import csv
+from typing import List
+
+def index_range(page: int, page_size: int) -> tuple:
+    """ Calculate the start and end
+        indexes for a given pagination
+    """
+    if page <= 1:
+        start_index = 0
+    else:
+        start_index = (page - 1) * page_size
+
+    end_index = start_index + page_size
+
+    return start_index, end_index
