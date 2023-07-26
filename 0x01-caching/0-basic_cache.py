654ef8f03cc6bdfc09599d8d6dd90f46e9859255
waley-code <oyewaleadeosun@yahoo.com>
2023-07-26 08:32:26 +0100

    fix pycodestyle for all files

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -3,22 +3,23 @@
 """
 from base_caching import BaseCaching
 
+
 class BasicCache(BaseCaching):
     """Basic caching class
     """
     def __init__(self):
-        super().__init__();
-    
+        super().__init__()
+
     def put(self, key, item):
         """Adds a key value pair"""
         if ((key is not None) and (item is not None)):
-            self.cache_data.update({key: item});
-    
+            self.cache_data.update({key: item})
+
     def get(self, key):
         """returns a value for a key"""
         if key is None:
-            return None;
+            return None
         returnKey = self.cache_data.get(key, None)
         if returnKey is None:
-            return None;
-        return returnKey;
+            return None
+        return returnKey
---
+++ 0x01-caching/0-main.py
@@ -0,0 +1,161 @@
+# #!/usr/bin/python3
+# """ 0-main """
+# BasicCache = __import__('0-basic_cache').BasicCache
+
+# my_cache = BasicCache()
+# my_cache.print_cache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.print_cache()
+# print(my_cache.get("A"))
+# print(my_cache.get("B"))
+# print(my_cache.get("C"))
+# print(my_cache.get("D"))
+# my_cache.print_cache()
+# my_cache.put("D", "School")
+# my_cache.put("E", "Battery")
+# my_cache.put("A", "Street")
+# my_cache.print_cache()
+# print(my_cache.get("A"))
+
+#!/usr/bin/python3
+# """ 1-main """
+# FIFOCache = __import__('1-fifo_cache').FIFOCache
+
+# my_cache = FIFOCache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.put("D", "School")
+# my_cache.print_cache()
+# my_cache.put("E", "Battery")
+# my_cache.print_cache()
+# my_cache.put("C", "Street")
+# my_cache.print_cache()
+# my_cache.put("F", "Mission")
+# my_cache.print_cache()
+
+#!/usr/bin/python3
+""" 2-main """
+# LIFOCache = __import__('2-lifo_cache').LIFOCache
+
+# my_cache = LIFOCache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.put("D", "School")
+# my_cache.print_cache()
+# my_cache.put("E", "Battery")
+# my_cache.print_cache()
+# my_cache.put("C", "Street")
+# my_cache.print_cache()
+# my_cache.put("F", "Mission")
+# my_cache.print_cache()
+# my_cache.put("G", "San Francisco")
+# my_cache.print_cache()
+
+
+#!/usr/bin/python3
+# """ 3-main """
+# LRUCache = __import__('3-lru_cache').LRUCache
+
+# my_cache = LRUCache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.put("D", "School")
+# my_cache.print_cache()
+# print(my_cache.get("B"))
+# my_cache.put("E", "Battery")
+# my_cache.print_cache()
+# my_cache.put("C", "Street")
+# my_cache.print_cache()
+# print(my_cache.get("A"))
+# print(my_cache.get("B"))
+# print(my_cache.get("C"))
+# my_cache.put("F", "Mission")
+# my_cache.print_cache()
+# my_cache.put("G", "San Francisco")
+# my_cache.print_cache()
+# my_cache.put("H", "H")
+# my_cache.print_cache()
+# my_cache.put("I", "I")
+# my_cache.print_cache()
+# my_cache.put("J", "J")
+# my_cache.print_cache()
+# my_cache.put("K", "K")
+# my_cache.print_cache()
+
+
+# #!/usr/bin/python3
+# """ 4-main """
+# MRUCache = __import__('4-mru_cache').MRUCache
+
+# my_cache = MRUCache()
+# my_cache.put("A", "Hello")
+# my_cache.put("B", "World")
+# my_cache.put("C", "Holberton")
+# my_cache.put("D", "School")
+# my_cache.print_cache()
+# print(my_cache.get("B"))
+# my_cache.put("E", "Battery")
+# my_cache.print_cache()
+# my_cache.put("C", "Street")
+# my_cache.print_cache()
+# print(my_cache.get("A"))
+# print(my_cache.get("B"))
+# print(my_cache.get("C"))
+# my_cache.put("F", "Mission")
+# my_cache.print_cache()
+# my_cache.put("G", "San Francisco")
+# my_cache.print_cache()
+# my_cache.put("H", "H")
+# my_cache.print_cache()
+# my_cache.put("I", "I")
+# my_cache.print_cache()
+# my_cache.put("J", "J")
+# my_cache.print_cache()
+# my_cache.put("K", "K")
+# my_cache.print_cache()
+
+#!/usr/bin/python3
+""" 100-main """
+LFUCache = __import__('100-lfu_cache').LFUCache
+
+my_cache = LFUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+print(my_cache.get("I"))
+print(my_cache.get("H"))
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
+my_cache.put("L", "L")
+my_cache.print_cache()
+my_cache.put("M", "M")
+my_cache.print_cache()
