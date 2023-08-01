b5d30c3c4a9172919421bc8e31d70f2040a5b0f9
waley-code <oyewaleadeosun@yahoo.com>
2023-08-02 00:44:21 +0100

    complete task 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+""" Basic Flask app """
+
+from flask import Flask, render_template, request
+from flask_babel import Babel, localeselector
+
+app = Flask(__name__)
+
+
+# Config class with available languages
+class Config:
+    """Config class with available languages"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+# Instantiate it in a module-level variable named babel
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """A basic route"""
+    return render_template('2-index.html')
+
+
+@babel.localeselector
+def get_locale():
+    """configure the language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
